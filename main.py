c08f23d8b49b423d969ceb3f4b3f2351c30fd75a
totowang-hhh <tt1224@hotmail.com>
2026-02-15 21:14:32 +0800

    Commented unnecessary code in test file

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import charmy as cm
 
 window = cm.Window(size=(300, 160))
 
-window.bind("resize", lambda event: print(f"<{event.event_type}>"))
-window.bind("move", lambda event: print(f"<{event.event_type}>"))
+# window.bind("resize", lambda event: print(f"<{event.event_type}>"))
+# window.bind("move", lambda event: print(f"<{event.event_type}>"))
 
 print(window)
 
