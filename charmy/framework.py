d555f6f90b4953ca5176b0118c5a6fa4ef60fb70
littlewhitecloud <q1141926647@163.com>
2026-02-18 13:01:08 +0800

    chore

--- charmy/framework.py
+++ charmy/framework.py
@@ -172,20 +172,20 @@ class GLFW(UIFramework):
         self.glfw.window_hint(self.glfw.CONTEXT_RELEASE_BEHAVIOR, self.glfw.RELEASE_BEHAVIOR_NONE)
 
         self.glfw.window_hint(self.glfw.STENCIL_BITS, 8)
-        if PLATFORM == "macos":
-            self.glfw.window_hint(self.glfw.COCOA_RETINA_FRAMEBUFFER, self.glfw.TRUE)  # macOS
-        else:
-            self.glfw.window_hint(self.glfw.SCALE_TO_MONITOR, self.glfw.TRUE)  # Windows/Linux
-
-        self.glfw.window_hint(self.glfw.CONTEXT_VERSION_MAJOR, 3)
 
         # see https://www.glfw.org/faq#macos
         if PLATFORM == "macos":
+            self.glfw.window_hint(self.glfw.COCOA_RETINA_FRAMEBUFFER, self.glfw.TRUE)
             self.glfw.window_hint(self.glfw.OPENGL_FORWARD_COMPAT, True)
+        else: # Windows/Linux
+            self.glfw.window_hint(self.glfw.SCALE_TO_MONITOR, self.glfw.TRUE)
+
+        self.glfw.window_hint(self.glfw.CONTEXT_VERSION_MAJOR, 3)
         self.glfw.window_hint(self.glfw.CONTEXT_VERSION_MINOR, 2 if PLATFORM == "macos" else 3)
 
         self.glfw.window_hint(self.glfw.OPENGL_PROFILE, self.glfw.OPENGL_CORE_PROFILE)
 
+        # create window
         window = self.glfw.create_window(size["width"], size["height"], title, None, None)
 
         if window == None:
