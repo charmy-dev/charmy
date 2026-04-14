6dd6c9f3087c7c07d184133af16aa92c411a0898
rgzz666 <tt1224@hotmail.com>
2026-04-14 13:47:50 +0800

    continue refactoring backend

--- charmy/backend/genesis.py
+++ charmy/backend/genesis.py
@@ -5,7 +5,12 @@
 
 # Under dev
 
-class Backend():
+import glfw
+import skia
+from . import template
+
+
+class Backend(template.Backend):
 
     def __init__(self):
         """Here goes the backend's metadata."""
@@ -13,3 +18,30 @@ class Backend():
         self.friendly_name = "Genesis (early development)"
         self.version = "0.1.0"
         self.author = ["XiangQinXi", "rgzz666"]
+
+    # part: window
+    
+    @staticmethod
+    def create_window(self, size, title, **kwargs) -> dict[str, typing.Any]:
+        # mystery optimize
+        self.glfw.window_hint(self.glfw.CONTEXT_RELEASE_BEHAVIOR, self.glfw.RELEASE_BEHAVIOR_NONE)
+
+        self.glfw.window_hint(self.glfw.STENCIL_BITS, 8)
+
+        # see https://www.glfw.org/faq#macos
+        if PLATFORM == "macos":
+            self.glfw.window_hint(self.glfw.COCOA_RETINA_FRAMEBUFFER, self.glfw.TRUE)
+            self.glfw.window_hint(self.glfw.OPENGL_FORWARD_COMPAT, True)
+        else:  # Windows/Linux
+            self.glfw.window_hint(self.glfw.SCALE_TO_MONITOR, self.glfw.TRUE)
+
+        self.glfw.window_hint(self.glfw.CONTEXT_VERSION_MAJOR, 3)
+        self.glfw.window_hint(self.glfw.CONTEXT_VERSION_MINOR, 2 if PLATFORM == "macos" else 3)
+
+        self.glfw.window_hint(self.glfw.OPENGL_PROFILE, self.glfw.OPENGL_CORE_PROFILE)
+
+        # create window
+        window = self.glfw.create_window(size.width, size.height, title, None, None)
+
+        if window == None:
+            raise RuntimeError("Can't create window")
--- charmy/backend/template.py
+++ charmy/backend/template.py
@@ -1,9 +1,29 @@
+import warnings
+
 class Backend():
     """This is a template of Backend, does not have any actual function."""
 
     def __init__(self):
-        """Here goes the backend's metadata."""
+        """Here goes the backend's metadata. In template, APIs are also aliased here."""
         self.name = "template"
-        self.friendly_name = "Fallback to none"
+        self.friendly_name = "No available backend"
         self.version = "0.0.0"
         self.author = []
+
+        # Make placeholders for APIs
+        func = self.placeholder_function # Just for aliasing
+
+        self.window_create = func               # Create win
+        self.window_set_title = func            # Set window title
+        self.window_set_icon = func             # Set window icon
+        self.window_set_size = func             # Set window size
+        self.window_set_scale_mode = func       # Set window DPI scale mode
+        self.window_set_background = func       # Set window background
+        self.window_set_alpha = func            # Set window transparency
+        self.window_set_state = func            # Set window state (e.g. minimized / normal)
+        self.window_set_fullscreen = func       # Set if window fullscreen
+        self.window_set_titlebar = func         # Set window titlebar state (Shown / Hidden)
+        self.window_edit = func                 # Edit other window properties provided by backend
+
+    def placeholder_function(self, *args, **kwargs):
+        warnings.warn(f"This function is not implemented in backend {self.friendly_name}.")
