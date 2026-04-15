37b1173c110c8b3de8ddb2ad6a18b273f8ffafb5
真_人工智障 <67090055+TotoWang-hhh@users.noreply.github.com>
2026-04-15 12:52:55 +0800

    Part of the WindowBase

--- charmy/backend/template.py
+++ charmy/backend/template.py
@@ -1,11 +1,15 @@
+import typing
+
+from dataclasses import dataclass
 import warnings
+
 
 class Backend():
     """This is a template of Backend, does not have any actual function."""
 
     def __init__(self):
-        """Here goes the backend's metadata. In template, APIs are also aliased here."""
-        self.name = "template"
+        """Here goes the backend's metadata. In nobackend, APIs are also aliased here."""
+        self.name = "nobackend"
         self.friendly_name = "No available backend"
         self.version = "0.0.0"
         self.author = []
@@ -15,17 +19,50 @@ class Backend():
 
         self.init=func                          # Initialize this backend
 
-        self.window_create = func               # Create win
-        self.window_set_title = func            # Set window title
-        self.window_set_icon = func             # Set window icon
-        self.window_set_size = func             # Set window size
-        self.window_set_scale_mode = func       # Set window DPI scale mode
-        self.window_set_background = func       # Set window background
-        self.window_set_alpha = func            # Set window transparency
-        self.window_set_state = func            # Set window state (e.g. minimized / normal)
-        self.window_set_fullscreen = func       # Set if window fullscreen
-        self.window_set_titlebar = func         # Set window titlebar state (Shown / Hidden)
-        self.window_edit = func                 # Edit other window properties provided by backend
-
     def placeholder_function(self, *args, **kwargs):
         warnings.warn(f"This function is not implemented in backend {self.friendly_name}.")
+
+
+@dataclass
+class SupportState():
+    """To flag which features this backend supports."""
+    pass
+
+
+@dataclass
+class WindowSupportState(SupportState):
+    """Represents support states of windows held by this backend."""
+    set_title           : bool = False
+    set_icon            : bool = False
+    resize              : bool = False
+    set_scale_mode      : bool = False
+    set_background      : bool = False
+    translucent         : bool = False
+    set_state           : bool = False
+    fullscreen          : bool = False
+    customize_titlebar  : bool = False
+
+class WindowBase():
+    """Base of the windows, abstracts window-level operations from the base UI lib."""
+    
+    def __init__(self):
+        self.supports: SupportState = WindowSupportState()
+
+        self.title: str = "Charmy Dummy Window"
+        self.icon: bytearray | None = None
+        self.size: tuple[int, int] = (0, 0)
+        self.scale_mode: str = "default_scale"
+        self.background: str | tuple[int, int, int] | typing.Any = "#ffffff"
+        self.alpha: float = 1
+        self.state: str = "normal"
+        self.fullscreen: bool = False
+        self.customize_titlebar = False
+
+        # And no need to perform any action to a dummy window
+    
+    def show(self):
+        """Shows the window, although not supported in nobackend so will raise an error."""
+        raise NotImplementedError(
+            "nobackend is not a valid backend to make Charmy works. "
+            "You must install another backend that supports your system GUI. "
+        )
