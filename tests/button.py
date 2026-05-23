a2b00c8085dae0da861c5aa04eb0226d3bb35e20
totowang-hhh <tt1224@hotmail.com>
2026-05-24 02:15:23 +0800

    Remove older codes from button demo

--- tests/button.py
+++ tests/button.py
@@ -1,19 +1,10 @@
 import charmy as cm
-# import time
 
 
 window = cm.Window(size=(300, 160))
 window.title = "Button test"
 
 button = cm.Button(window, text="Hit me")
-# button.place((10, 10), (100, 50))
-# button.draw((10, 10), (100, 50))
 button.place((10, 10))
-# window.bind("mouse_press", lambda e: print(e["mods"]))
 
-
-# while True:
-#     # time.sleep(0.5)
-#     # button.draw()
-#     window.parent.update()
 cm.mainloop()
