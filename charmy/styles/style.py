300df8cf53263c199b4e0bdb3bb516b9a77fb1fa
totowang-hhh <tt1224@hotmail.com>
2026-06-08 12:01:55 +0800

    Added TODO in style

--- charmy/styles/style.py
+++ charmy/styles/style.py
@@ -38,6 +38,7 @@ def fill_vars(style_value: _typing.Any,
             if DEBUG_FLAGS.FILL_VARS_DEBUG_OUTPUT:
                 print(f"{requested_vars[0]} -> {eval(requested_vars[0][2:-1])}")
             return eval(requested_vars[0][2:-1])
+            # TODO: Refactor styles system and avoid unsafe eval() like this
         else:
             result = style_value
             for var in requested_vars:
