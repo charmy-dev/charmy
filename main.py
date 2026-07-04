d958b81917408deaaeb2bcc73282387ff75255f5
totowang-hhh <tt1224@hotmail.com>
2026-07-04 22:22:39 +0800

    Fix main.py demo

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 from charmy import Button, Window, mainloop
 
-window1 = Window(size=(300, 400))
+window1 = Window(size=(300, 200))
 window1.title = "Charmy GUI"
 
 with window1:
-    Button().place(0, 0, 100, 100)
-    Button().place(100, 100, 100, 100)
+    Button().place((0, 0), (100, 100))
+    Button().place((100, 100), (100, 100))
 
 mainloop()
