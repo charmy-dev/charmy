44b4ec68a4a7f3f88e183600cbaa7b231f030dea
totowang-hhh <tt1224@hotmail.com>
2026-07-09 14:41:54 +0800

    type checking features test used to find a represention for a specific case, perhaps will remove later as it is useless

--- tests/pylance_abilities.py
+++ tests/pylance_abilities.py
@@ -1,5 +1,55 @@
-from charmy import CharmyObject, EventHandling
-import reactive_caching
+"""DO NOT RUN THIS FILE DIRECTLY!!!"""
 
-class Widget(CharmyObject, EventHandling, reactive_caching.CachedClass):
-    pass
+from typing import ClassVar, cast, TypeAlias, Self, Protocol, TypeVar
+
+class WidgetProfile: ...
+class ButtonProfile(WidgetProfile):
+    dummy_attr: int = 5
+
+
+# 1. cast
+class WidgetA:
+    Profile: ClassVar[type[WidgetProfile]] = WidgetProfile
+
+    def foo(self) -> WidgetProfile:
+        result = WidgetProfile()
+        return cast(type(self).Profile, result)
+
+
+# 2. TypeAlias
+class WidgetB:
+    Profile: TypeAlias = WidgetProfile
+
+    def foo(self) -> Profile:
+        ...
+
+class ButtonB(WidgetB):
+    Profile: TypeAlias = ButtonProfile
+
+r = ButtonB().foo()
+r.dummy_attr
+
+
+# 3. Self
+class Widget:
+    Profile = WidgetProfile
+
+    def foo(self: Self):
+        reveal_type(self)
+        reveal_type(type(self))
+        reveal_type(self.Profile)
+
+
+# 4. Protocol
+class HasProfile(Protocol):
+    Profile: ClassVar[type[WidgetProfile]]
+
+def f(x: HasProfile):
+    reveal_type(x.Profile)
+
+
+# 5.
+P = TypeVar("P", bound=WidgetProfile)
+
+class Widget:
+    Profile: type[P]
